4720aa2b2395752fcbd5500573221b3cfad81d93
Niatechnologie <56614857+Niatechnologie@users.noreply.github.com>
2026-02-19 06:56:28 +0000

    update

--- src/routes/webradio/RadioCard.svelte
+++ src/routes/webradio/RadioCard.svelte
@@ -14,7 +14,7 @@
 	// Chemin de la pochette
 	const pochettePath = $derived(
 		radio.repertoire 
-			? `https://oneradiomobile.oneradio.ci/${radio.repertoire}/${radio.pochette}` 
+			? `https://oneradiomobile.oneradio.ci/image/${radio.pochette}` 
 			: `https://oneradiomobile.oneradio.ci/image/${radio.pochette}`
 	);
 
