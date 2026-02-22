f2c6c2e962bed92d005468da05fca2713fa76cd6
Niatechnologie <56614857+Niatechnologie@users.noreply.github.com>
2026-02-22 18:29:04 +0000

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -724,7 +724,7 @@
 
   /* Contenu principal */
   main {
-    margin-top: 70px !important; /* Espace pour la navbar fixe */
+    margin-top: 90px !important; /* Espace pour la navbar fixe */
     padding: 0;
     margin-bottom: 0;
   }
@@ -733,7 +733,7 @@
 
 @media (max-width: 868px) {
     main {
-      margin-top: 80px !important; /* Espace pour la navbar fixe */
+      margin-top: 100px !important; /* Espace pour la navbar fixe */
    
     }
     nav {
