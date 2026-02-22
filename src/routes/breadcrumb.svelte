ec2621c1af83bb7ef99e6c51ef89e0cfa3c66c8e
Niatechnologie <56614857+Niatechnologie@users.noreply.github.com>
2026-02-22 20:21:06 +0000

    update

--- src/routes/breadcrumb.svelte
+++ src/routes/breadcrumb.svelte
@@ -66,7 +66,7 @@
     padding: 0.75rem 1.5rem;
     border-bottom: 2px solid #ff1919;
     font-family: 'Roboto', sans-serif;
-    margin-top: 95px;
+    margin-top: 105px;
   }
 
   .breadcrumb-container {
