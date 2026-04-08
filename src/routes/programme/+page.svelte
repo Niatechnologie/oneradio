2c658ee7c0941acf47851cc9688262556f8c340c
Niatechnologie <56614857+Niatechnologie@users.noreply.github.com>
2026-04-08 15:28:12 +0000

    update

--- src/routes/programme/+page.svelte
+++ src/routes/programme/+page.svelte
@@ -67,9 +67,12 @@ function getAbbreviatedDay(day) {
         <span style=" padding-top:5px" >Suivez nous en direct</span>
           <a href="tel:+2250501877877" class="phone-number">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07A19.5 19.5 0 0 1 4.69 12 19.79 19.79 0 0 1 1.6 3.42 2 2 0 0 1 3.57 1h3a2 2 0 0 1 2 1.72c.127.96.361 1.903.7 2.81a2 2 0 0 1-.45 2.11L7.91 8.54a16 16 0 0 0 6 6l.91-.91a2 2 0 0 1 2.11-.45c.907.339 1.85.573 2.81.7A2 2 0 0 1 21.46 16z"></path></svg>
-            (+225) 0501 877 877
+            (+225) 0500 877 877
           </a>
           <div class="social-links">
+           <a href="https://wa.me/2250501877877" target="_blank" rel="noopener noreferrer" class="social-link whatsapp" aria-label="WhatsApp">
+             <i class="bi bi-whatsapp"></i>
+           </a>
             <a href="https://www.facebook.com/oneradioci" target="_blank" rel="noopener noreferrer" class="social-link facebook" aria-label="Facebook">
               <i class="bi bi-facebook"></i>
             </a>
@@ -240,6 +243,7 @@ function getAbbreviatedDay(day) {
     .social-link.youtube  { background: #FF0000; }
     .social-link.facebook { background: #1877F2; }
     .social-link.tiktok   { background: #000000; }
+    .social-link.whatsapp { background: #25D366; }
   
     .radio-icon {
       width: 2rem;
