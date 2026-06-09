74a75fc6230a50c293351faed6ee2d6e109ad9f9
Niatechnologie <56614857+Niatechnologie@users.noreply.github.com>
2026-06-09 08:58:08 +0000

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -401,7 +401,7 @@
                 responsive: {
                     0: { items: 1 },
                     768: { items: 2 },
-                    1000: { items: 3 }
+                    1000: { items: 4 }
                 }
             });
         }
