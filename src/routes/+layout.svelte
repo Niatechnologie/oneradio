d39aa51458ea71994fb5c4bf9d4c0f507c52bd2f
Niatechnologie <niaentreprise@gmail.com>
2026-07-13 11:54:15 +0000

    Set mobile player timer to sit at most 10px from the volume button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1082,7 +1082,7 @@ async function fetchNowPlaying() {
   @media (max-width: 767px) {
     .audio-player { height: 7rem; width: 80%; }
     .equalizer-container { display: none !important; }
-    .progress-container { right: -32px; }
+    .progress-container { right: 10px; }
   }
   @media (max-width: 567px) {
     .audio-player { height: 5rem; width: 95%; }
